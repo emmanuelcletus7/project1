720708b5ae79d723ba70060f0a4ad35d1f7b6dea
emmanuelcletus7 <emmanuelcletus751@gmail.com>
2023-04-09 20:44:06 +0100

    working on the dashboard

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.html", "./userfolder/**/*.html", "./adminfolder/**/*.html"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
